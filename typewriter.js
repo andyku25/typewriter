3a36620a9dce69e861e4e069783e4a3e2122e802
Andrew <andrew.ku93@gmail.com>
2021-02-09 18:06:29 +0000

    initial commit with setTimeout typewriter

---
+++ typewriter.js
@@ -0,0 +1,8 @@
+const sentence = "hello there from lighthouse labs";
+let interval = 0;
+
+for (const char of sentence) {
+  setTimeout(() => process.stdout.write(char), interval);
+  interval += 50;
+}
+setTimeout(() => console.log(), interval);
